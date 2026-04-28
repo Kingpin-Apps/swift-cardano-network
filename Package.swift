fe345be14033b8946014bb0f34e9e5ca24892042
Hareem Adderley <hadderley@kingpinapps.com>
2026-04-28 09:32:07 -0500

    fix: require swift-log >= 1.7.0 for LogEvent, FileLogHandler and SystemLogHandler

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.22.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.5.0"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.15"),
     ],
